8782cff3ea5b7e6dc8d3bbbdfe3cb843c859a9b4
fdhg0 <fdhg0@FernandoAsus-M16>
2024-07-19 09:27:30 -0600

    Primera parte ejericio Lab

---
+++ src/hernandez_fernando_variablesejercicio/java/Hernandez_Fernando_VariablesEjercicioJava.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hernandez_fernando_variablesejercicio.java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fdhg0
+ */
+public class Hernandez_Fernando_VariablesEjercicioJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Conseguir informacion del empleado
+        Scanner lea=new Scanner(System.in);
+        System.out.println("Ingresar Nombre del Empleado: ");
+        String nombreEmpleado= lea.next();
+        System.out.println("Hora de Trabajo Mensual: ");
+        int horaM =lea.nextInt();
+        System.out.println("Tarifa por Hora: ");
+        double Thora=lea.nextDouble();
+        System.out.println("Ingrese Salario del Empleado Semanal: ");
+     
+        double Salaempleado =(horaM * Thora)/4;
+        
+        System.out.println("----Boleta Del Empleado----"+
+                "\n Nombre del Empleado: "+nombreEmpleado+
+                "\n Hora de trabajo Mensual: "+horaM+ 
+                "\n Tarifa por Hora: "+Thora+
+                "\n Salario del empleado semanal: "+Salaempleado+"");
+        
+        }
+}
---
+++ src/hernandez_fernando_variablesejercicio/java/Planilla_de_Empleado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hernandez_fernando_variablesejercicio.java;
+
+/**
+ *
+ * @author fdhg0
+ */
+public class Planilla_de_Empleado {
+    public static void main(String []args){
+        
+        
+    }
+}
