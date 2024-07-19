6437d242642cf75f33fc5c88055d306c436f309c
fdhg0 <fdhg0@FernandoAsus-M16>
2024-07-19 11:56:29 -0600

    Ultima parte de lab

---
+++ src/hernandez_fernando_variablesejercicio/java/Formulas_Fernando_Hernandez.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hernandez_fernando_variablesejercicio.java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fdhg0
+ */
+public class Formulas_Fernando_Hernandez {
+     public static void main(String []args){
+        Scanner lea=new Scanner(System.in);
+         double ejercicio_a =((3/2)+(4/3));
+         System.out.println("ejercicio a: "+ejercicio_a+"");
+         
+         System.out.println("elije valor de variable x ");
+         double ValoX =lea.nextDouble();
+         System.out.println("elije valor de variable y ");
+         double ValoY =lea.nextDouble();
+         double ejercicio_b =(1/(ValoX-5)-((3*ValoX*ValoY)/4));
+         System.out.println("ejercicio b: "+ejercicio_b+"");
+         
+         double ejercicio_c=(1/2)+7;
+         System.out.println("ejercicio c: "+ejercicio_c+"");
+         
+         double ejercicio_d= 7+(1/2);
+         System.out.println("ejercicio d: "+ejercicio_d+"");
+         
+         System.out.println("elije valor de variable a ");
+         double ValoA =lea.nextDouble();
+         System.out.println("elije valor de variable b ");
+         double ValoB =lea.nextDouble();
+         System.out.println("elije valor de variable c ");
+         double ValoC =lea.nextDouble();
+         System.out.println("elije valor de variable d ");
+         double ValoD =lea.nextDouble();
+         System.out.println("elije valor de variable e ");
+         double ValoE =lea.nextDouble();
+         System.out.println("elije valor de variable f ");
+         double ValoF =lea.nextDouble();
+         System.out.println("elije valor de variable g ");
+         double ValoG =lea.nextDouble();
+         System.out.println("elije valor de variable h ");        
+         double ValoH =lea.nextDouble(); 
+         System.out.println("elije valor de variable j ");
+         double ValoJ =lea.nextDouble();
+         
+         double ejercicio_e=(ValoA)^2)/ValoB-ValoC)+(ValoD-ValoE)/ValoF-((ValoG*ValoH)/ValoJ)));
+         System.out.println("ejercicio e: "+ejercicio_e+"");
+
+     }  
+}
+
